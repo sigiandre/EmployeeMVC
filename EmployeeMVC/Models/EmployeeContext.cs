c3e832a234875937b9ba9d94ba2e5884716d3038
Andre Diaz <andrediaz@MacBook-Pro-de-Andre.local>
2021-01-10 06:19:11 -0500

    first commit

---
+++ EmployeeMVC/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeMVC.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options):base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
